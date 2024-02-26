4a70c9d2df65314bf0cd6fc9ae8522fa87d7e1e5
jzepka <161222490+jzepka@users.noreply.github.com>
2024-02-26 12:57:24 +0100

    Add files via upload

---
+++ js/works-img-pt.js
@@ -0,0 +1,10 @@
+var images = ["img/boilerroom/1.jpg", "img/come/2.jpg", "img/overview/img9.jpg", "img/les_coureurs.png", "img/ghost/img9.png", "img/rhein.png", "img/photos/photo3.jpg", "img/img17.jpg", "img/gardens/img15.png", "img/photos/photo4.jpg", "img/boilerroom/4.JPG", "img/photos/photo5.jpg",  "img/photos/photo1.jpg", "img/overview/img15.jpg", "img/ghost/home/7.png", "img/boilerroom/11.JPG", "img/img22.jpg"];
+
+var imgState = 0;
+
+var imgTag = document.getElementById("imgClickAndChange");
+
+imgTag.addEventListener("click", function (event) {
+  imgState = (++imgState % 17);
+  event.target.src = images[imgState];
+});
